c3abc7a70c47e82a52648d126e0253077b98d8d2
carlo <k4rl85@hotmail.com>
2014-05-30 13:01:42 +0200

    Created command line interpreter stub

--- client/client_cmdmanager.py
+++ client/client_cmdmanager.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+
+import cmd
+import requests
+
+
+class CommandParser(cmd.Cmd):
+    """Command line interpeter
+    Parse user input"""
+
+    prompt = '(Share)>>>'
+
+    def do_google(self, line):
+    """Print Google sourcepage"""
+        r = requests.get("http://www.google.it")
+        print r.text
+
+    def do_quit(self, line):
+    """Exit Command"""
+        return True
+
+    def do_EOF(self, line):
+        return True
+
+
+if __name__ == '__main__':
+    CommandParser().cmdloop()
