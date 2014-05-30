b4061ec79c8569c0a1a6a02a60fb0d010197eeff
Fabrizio Fantozzi <fabry_fa84@yahoo.it>
2014-05-30 18:12:16 +0200

    - Added Create new user functionality  - Created a simple unit test (work in progress)

--- client/client_cmdmanager.py
+++ client/client_cmdmanager.py
@@ -2,27 +2,48 @@
 #-*- coding: utf-8 -*-
 
 import cmd
-import requests
-
+# import requests
+import fake_request as requests
 
 class CommandParser(cmd.Cmd):
-    """Command line interpeter
+    """Command line interpreter
     Parse user input"""
 
     prompt = '(Share)>>>'
 
     def do_google(self, line):
-    """Print Google sourcepage"""
+        """Print Google source page"""
         r = requests.get("http://www.google.it")
         print r.text
 
     def do_quit(self, line):
-    """Exit Command"""
+        """Exit Command"""
         return True
 
     def do_EOF(self, line):
         return True
 
+    def do_newUser(self, line):
+        """ Create new User
+            Usage: newUser <username> <password>
+        """
+        data = {}
+
+        try:
+            user, password = line.split()
+            data['user'] = user
+            data['pass'] = password
+        except ValueError:
+            print 'bad arguments'
+        else:
+            r = requests.post('NEWUSER', data)
+            try:
+                r.raise_for_status()
+            except: # (work in progess) must catch specific exception
+                print r.reason
+
 
 if __name__ == '__main__':
     CommandParser().cmdloop()
+
+
