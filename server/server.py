3e040389b9a9672c3f830e3a11c765b0335132f6
Paolo Rossi <user@commodore64.(none)>
2014-05-30 15:38:26 +0200

    Basic access authentication functions

--- server/server.py
+++ server/server.py
@@ -1,2 +1,37 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+
+from flask import Flask
+from flask.ext.httpauth import HTTPBasicAuth
+
+
+URL_PREFIX = '/API/V1'
+
+
+user_login_info = {
+    "luca": "luca",
+    "iacopo": "iacopo"
+}
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+
+@auth.get_password
+def get_pw(username):
+    return user_login_info.get(username)
+
+
+@app.route(URL_PREFIX)
+def hello():
+    return "Autenticazione non necessaria"
+
+
+@app.route("{}/test".format(URL_PREFIX))
+@auth.login_required
+def index():
+    return "ROUTE TEST - Autenticato come utente: %s!" % auth.username()
+
+
+if __name__ == "__main__":
+    app.run()
