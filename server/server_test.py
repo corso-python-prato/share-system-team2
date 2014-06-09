46d7b6d091a799616bdeee838428135b975ef764
hiryuken <alessandro.diaco@gmail.com>
2014-06-09 09:38:21 +0200

    Corrected the assertTrue in assertEqual for each Status Code.

--- server/server_test.py
+++ server/server_test.py
@@ -9,26 +9,26 @@ class TestRequests(unittest.TestCase):
 
     def test_files_get_with_auth(self):
         test = self.app.get("/API/V1/files/file1.txt")
-        self.assertTrue("200" in str(test))
+        self.assertEqual(test.status_code, 200)
 
     def test_files_get_without_auth(self):
         test = self.app.get("/API/V1/files/file1.txt")
-        self.assertTrue("200" in str(test))
+        self.assertEqual(test.status_code, 200)
 
     def test_files_get_with_not_existing_file(self):
         test = self.app.get("/API/V1/files/file1.cat")
-        self.assertTrue("500" in str(test))
+        self.assertEqual(test.status_code, 404)
 
     def test_files_post_with_auth(self):
         test = self.app.post("/API/V1/files/test/myfile.dat", data=dict(
             file=(io.BytesIO(b"this is a test"), "test.pdf"),), follow_redirects=True)
-        self.assertTrue("201" in str(test))
+        self.assertEqual(test.status_code, 201)
         self.assertTrue(os.path.isfile("upload/test/myfile.dat"))
 
     def test_files_post_with_not_allowed_path(self):
         test = self.app.post("/API/V1/files/../../../test/myfile2.dat", data=dict(
             file=(io.BytesIO(b"this is a test"), "test.pdf"),), follow_redirects=True)
-        self.assertTrue("403" in str(test))
+        self.assertEqual(test.status_code, 403)
         self.assertFalse(os.path.isfile("upload/../../../test.myfile2.dat"))
 
 
