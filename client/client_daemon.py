188607294bebeaa80ac5b8e152935a3f0370ef3f
carlo <k4rl85@hotmail.com>
2014-06-13 15:21:28 +0200

    Implemented first base for download_files_state in client_daemon, this module will download the server_state and update it with client state

--- client/client_daemon.py
+++ client/client_daemon.py
@@ -110,9 +110,33 @@ class Daemon(object):
             print "No config File!"
             exit()           
         self.conn_mng = connection_manager.ConnectionManager(self.cfg)
+        self.search_diff_from_server(self.cfg['sharing_path'])
         self.dir_manager = DirectoryMonitor(self.cfg['sharing_path'], self.event_dispatcher)
         self.running = 0
 
+    def search_diff_from_server(self, sharing_path):
+        """
+        download from server the files state and find the difference from actual state
+        """
+        def download_files_state():
+            """download from server the saved files_state"""
+            #TODO implement the real function
+            pass
+
+        files_state = {self.cfg['sharing_path'] : '<md5>'}
+        for dirpath, dirs, files in os.walk(self.cfg['sharing_path']):
+            for filename in files:
+                file_path = os.path.join(dirpath, filename)
+                if file_path in files_state:
+                    # Open file and calculate md5. TODO: catch and handle os errors.
+                    with open(file_path, 'rb') as fp:
+                        md5_string = calculate_file_md5(fp)
+                        print md5_string
+                    if files_state[file_path] != md5_string:
+                        pass #TODO files/aggiorna
+                else:
+                    print "creato nuovo file"
+                    pass#TODO files/crea
 
     def cmd_dispatcher(self, data):
         """
