58a03747ea348152745c1b8273aaad6ab0f42092
pasqualeeatsjobs <pasquale.mangialavori@gmail.com>
2014-06-20 11:17:00 +0200

    Added calculate_md5_of_dir function

--- client/client_daemon.py
+++ client/client_daemon.py
@@ -8,6 +8,8 @@ import select
 import os
 import hashlib
 import re
+import time
+import traceback
 
 # we import PollingObserver instead of Observer because the deleted event
 # is not capturing https://github.com/gorakhargosh/watchdog/issues/46
@@ -265,6 +267,41 @@ class Daemon(RegexMatchingEventHandler):
         self.observer.join()
         self.listener_socket.close()
         self.running = 0
+
+    def calculate_md5_of_dir(directory, verbose=0):
+        """
+        Calculate the md5 of the entire directory
+        """
+        start = time.time()
+        md5Hash = hashlib.md5()
+        if not os.path.exists (directory):
+            return -1
+        try:
+            for root, dirs, files in os.walk(directory):
+                for names in files:
+                    if verbose == 1:
+                        print 'Calculate MD5 of: ', names
+                    filepath = os.path.join(root,names)
+                try:
+                    f1 = open(filepath, 'rb')
+                except:
+                # You can't open the file for some reason
+                    f1.close()
+                    continue
+
+            while 1:
+            # Read file in as little chunks
+                buf = f1.read(4096)
+                if not buf : break
+                md5Hash.update(hashlib.md5(buf).hexdigest())
+            f1.close()
+        except:
+            # Print the stack traceback
+            traceback.print_exc()
+            return -2
+        stop = time.time()
+        print stop - start
+        return md5Hash.hexdigest()
 
 
 if __name__ == '__main__':
