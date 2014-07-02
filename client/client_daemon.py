c8db52b8a188fc5b19066a1c15c8330ca625cdb9
iacopy <iacopomarmorini@gmail.com>
2014-07-02 23:34:30 +0200

    fixed assignment daemon_state = 'down'

--- client/client_daemon.py
+++ client/client_daemon.py
@@ -609,7 +609,7 @@ class Daemon(RegexMatchingEventHandler):
         """
         if self.daemon_state == 'started':
             self.running = 0
-            self.daemon_state == 'down'
+            self.daemon_state = 'down'
         self.save_local_dir_state()
         if exit_message:
             print exit_message
