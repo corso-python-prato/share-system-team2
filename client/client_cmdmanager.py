1e10e0bd0b7bf9a037598e0c2ac49604fb3f3ec4
Fabrizio Fantozzi <fabry_fa84@yahoo.it>
2014-07-07 11:53:25 +0200

    Refactor CommandParser to improve connection testing

--- client/client_cmdmanager.py
+++ client/client_cmdmanager.py
@@ -20,6 +20,10 @@ class CommandParser(cmd.Cmd):
     # Override attribute in cmd.Cmd
     prompt = '(PyBox)>>> '
 
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     def _send_to_daemon(self, message=None):
         """
         it sends user input command to the daemon server
@@ -28,37 +32,41 @@ class CommandParser(cmd.Cmd):
             raise
 
         data_packet = json.dumps(message)
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            s.connect((DAEMON_HOST, DAEMON_PORT))
-
             # send the command to daemon
             data_packet_size = struct.pack('!i', len(data_packet))
-            s.sendall(data_packet_size)
-            s.sendall(data_packet)
+            self.sock.sendall(data_packet_size)
+            self.sock.sendall(data_packet)
 
             # receive the information message from daemon
-            response_size = s.recv(struct.calcsize('!i'))
+            response_size = self.sock.recv(struct.calcsize('!i'))
             if len(response_size) == struct.calcsize('!i'):
                 response_size = int(struct.unpack('!i', response_size)[0])
                 response_packet = ''
                 remaining_size = response_size
                 while len(response_packet) < response_size:
-                    response_buffer = s.recv(remaining_size)
+                    response_buffer = self.sock.recv(remaining_size)
                     remaining_size -= len(response_buffer)
                     response_packet = ''.join([response_packet, response_buffer])
 
                 response = json.loads(response_packet)
 
                 print response['message']
+
+                # to improve testing
+                return response['message']
             else:
                 raise Exception('Error: lost connection with daemon')
 
-            s.close()
         except socket.error as ex:
             # log exception message
             print 'Socket Error: ', ex
+
+    def preloop(self):
+        self.sock.connect((DAEMON_HOST, DAEMON_PORT))
+
+    def postloop(self):
+        self.sock.close()
 
     def do_quit(self, line):
         """Exit Command"""
