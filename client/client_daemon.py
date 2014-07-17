d281c3d7215c7379ad661f5474fb5ec288dc942e
eatsjobs <pasquale.mangialavori@gmail.com>
2014-07-17 12:22:53 +0200

    at every call to server i have to update the last_operation_timestamp

--- client/client_daemon.py
+++ client/client_daemon.py
@@ -440,7 +440,7 @@ class Daemon(RegexMatchingEventHandler):
         sync_commands = self._sync_process(server_timestamp, files)
 
         # Initialize the variable where we put the timestamp of the last operation we did
-        last_operation_timestamp = None
+        last_operation_timestamp = server_timestamp
 
         # makes all synchronization commands
         for command, path in sync_commands:
@@ -468,15 +468,12 @@ class Daemon(RegexMatchingEventHandler):
                 self.observer.skip(self.absolutize_path(path))
                 connection_result = self.conn_mng.dispatch_request(command, {'filepath': path})
                 if connection_result:
-                    if not last_operation_timestamp:
-                        last_operation_timestamp = server_timestamp
                     print 'Downloaded file with path "{}" INTO SYNC'.format(path)                    
                     self.client_snapshot[path] = files[path]
                 else:
                     self.stop(1, 'Error during connection with the server. Client fail to "download" this file: {}'.format(path))
 
-        if last_operation_timestamp:
-            self.update_local_dir_state(last_operation_timestamp)
+        self.update_local_dir_state(last_operation_timestamp)
 
     def relativize_path(self, abs_path):
         """
