ea611c991de91d0b995cd66a262710f828a5a5e2
iacopy <iacopomarmorini@gmail.com>
2014-07-28 12:12:31 +0200

    Several 'validate_email' test cases added. 'consecutive_dots_in_domain_part' currently fail.

--- client/test_cmdmanager.py
+++ client/test_cmdmanager.py
@@ -239,8 +239,32 @@ class TestRecoverpassEmailAndToken(unittest.TestCase):
 
 
 class TestValidateEmail(unittest.TestCase):
+    def test_no_local_part(self):
+        self.assertFalse(client_cmdmanager.validate_email('@gmail.com'))
+
+    def test_no_domain(self):
+        self.assertFalse(client_cmdmanager.validate_email('address@'))
+
+    def too_many_at(self):
+        self.assertFalse(client_cmdmanager.validate_email('address@@gmail.com'))
+
+    def dot_at_begin_or_end(self):
+        self.assertFalse(client_cmdmanager.validate_email('.address@gmail.com'))
+        self.assertFalse(client_cmdmanager.validate_email('address.@gmail.com'))
+
+    def test_no_dots_in_domain(self):
+        self.assertFalse(client_cmdmanager.validate_email('address@gmailcom'))
+
     def test_too_many_dots_in_local_part(self):
         self.assertFalse(client_cmdmanager.validate_email('too.many.dots@gmail.com'))
+
+    def test_consecutive_dots_in_domain_part(self):
+        self.assertFalse(client_cmdmanager.validate_email('address@gmail..com'))
+
+    def test_invalid_characters(self):
+        for invalid in '^$/:?#[]':
+            self.assertFalse(client_cmdmanager.validate_email('my{}address@gmail.com'.format(invalid)))
+
 
 if __name__ == '__main__':
     unittest.main()
