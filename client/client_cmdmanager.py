574b0c228b533ec2db838154e1ea1713b4c0900d
iacopy <iacopomarmorini@gmail.com>
2014-07-28 08:27:09 +0200

    bugfix: regular expression for email validation fixed (it was too permissive, accepting every kind of characters, due to improper use of '\b')

--- client/client_cmdmanager.py
+++ client/client_cmdmanager.py
@@ -19,8 +19,8 @@ daemon_port = 50001
 
 # A regular expression to check if an email address is valid or not.
 # WARNING: it seems a not 100%-exhaustive email address validation.
-# source: http://www.regular-expressions.info/email.html
-EMAIL_REG_OBJ = re.compile(r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b', re.IGNORECASE)
+# source: http://www.regular-expressions.info/email.html (modified)
+EMAIL_REG_OBJ = re.compile(r'^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$', re.IGNORECASE)
 
 
 def load_cfg(cfg_path=CONFIG_FILEPATH):
