a948a381c4ac94a9fdd4c7333e96863da2a0c602
k4rl85 <k4rl85@hotmail.com>
2014-09-19 12:32:25 +0200

    Added keyring implementation in connection_manager.
    Signed-off-by: k4rl85 <k4rl85@hotmail.com>

--- client/connection_manager.py
+++ client/connection_manager.py
@@ -22,6 +22,7 @@ import requests
 import json
 import os
 import logging
+import keyring
 
 
 class ConnectionManager(object):
@@ -62,7 +63,7 @@ class ConnectionManager(object):
         :param cfg: Dictionary where is contained the configuration
         """
         self.cfg = cfg
-        self.auth = (self.cfg.get('user', None), self.cfg.get('pass', None))
+        self.auth = (self.cfg.get('user'), keyring.get_password('PyBox', self.cfg.get('user', '')))
 
         # example of self.base_url = 'http://localhost:5000/API/V1/'
         self.base_url = ''.join([self.cfg['server_address'], self.cfg['api_suffix']])
