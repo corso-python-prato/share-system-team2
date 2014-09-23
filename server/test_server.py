2ddad4cd1586bcfafe6fa94eb1a5b98ed7894aa7
hiryuken <alessandro.diaco@gmail.com>
2014-09-23 12:48:35 +0200

    Implemented delete folder share test

--- server/test_server.py
+++ server/test_server.py
@@ -1319,6 +1319,15 @@ class TestShares(unittest.TestCase):
         self.assertNotIn(SHAREUSR, server.userdata[USR]['shared_with_others'][sharedFile])
         self.assertNotIn(sharedFile, server.userdata[SHAREUSR]['shared_with_me'][USR])
 
+    def test_delete_shared_folder(self):
+        sharedFolder = 'Music'
+        q = urlparse.urljoin(SERVER_SHARES_API, sharedFolder + '/' + SHAREUSR)
+        test = self.app.post(q, headers=make_basicauth_headers(USR, PW))
+        sharedFileRealPath = userpath2serverpath(os.path.join(USR,sharedFolder))
+        q = urlparse.urljoin(SERVER_SHARES_API, sharedFolder + '/' + SHAREUSR)
+        test = self.app.delete(q, headers=make_basicauth_headers(USR, PW))
+        self.assertNotIn(SHAREUSR, server.userdata[USR]['shared_with_others'][sharedFolder])
+        self.assertNotIn(sharedFolder, server.userdata[SHAREUSR]['shared_with_me'][USR])
 
 if __name__ == '__main__':
     unittest.main()
